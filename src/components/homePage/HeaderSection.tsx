b1a24b44d775a195dddd677f615d67bf1ee161eb
Yulia Gutorova <j.gutorova@gmail.com>
2023-02-25 22:01:50 +0100

    new main photo

--- src/components/homePage/HeaderSection.tsx
+++ src/components/homePage/HeaderSection.tsx
@@ -8,7 +8,7 @@ const HeaderSection = () => {
                 <div className='header-section-content'>
 
                     <h1 className='header-section-title'>Stockholm's premier home cleaning service</h1>
-                    <p className='header-section-text'>Hire us! Geet your home cleaned. We are a professional cleaning company offering all types od cleaning services.</p>
+                    <p className='header-section-text'>Hire us! Get your home cleaned. We are a professional cleaning company offering all types od cleaning services.</p>
                 </div>
 
                 <div>
