fb2f9a0ccfff43dc2fa25b475c616e9f98088594
Yulia Gutorova <j.gutorova@gmail.com>
2023-02-25 21:56:36 +0100

    new main photo

--- src/components/homePage/ContactUsSection.tsx
+++ src/components/homePage/ContactUsSection.tsx
@@ -4,25 +4,31 @@ const ContactUsSection = () => {
     return (
         <div className='contact-wrapper'>
             <div className='contact-card call-us'>
-                <h2 className='contact-title'>Call us now!</h2>
+            <i className="fa-solid fa-phone-volume"></i>
+
+                <p className='contact-title'>Call us now!</p>
                 <div className="contact-content">
-                <i className="fa-solid fa-phone-volume"></i>
+                
                 <p className='contact-text'>8-8000-123-00-00</p>
                 </div>
             </div>
 
             <div className='contact-card open'>
-                <h2 className='contact-title'>We are open 24/7!</h2>
+            <i className="fa-solid fa-clock"></i>
+            
+                <p className='contact-title'>We are open every day</p>
                 <div className="contact-content">
-                <i className="fa-solid fa-clock"></i>
+                
                 <p className='contact-text'>8:00 - 21:00</p>
                 </div>
             </div>
 
             <div className='contact-card address'>
-                <h2 className='contact-title'>Our main office</h2>
+            <i className="fa-solid fa-location-dot"></i>
+
+                <p className='contact-title'>Our main office</p>
                 <div className="contact-content">
-                <i className="fa-solid fa-location-dot"></i>
+                
                 <p className='contact-text'>Drottningsgatan 1, Stockholm</p>
                 </div>
             </div>            
