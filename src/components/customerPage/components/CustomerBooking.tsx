093791eaaee15607ed7c760ec42afd15fa6cbd3a
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-03-14 23:04:51 +0100

    Added information to customer sections

--- src/components/customerPage/components/CustomerBooking.tsx
+++ src/components/customerPage/components/CustomerBooking.tsx
@@ -4,7 +4,35 @@ function CustomerBooking () {
 
     return(
         <div className="booking-container">
-            <h2>Book Appointment</h2>
+            
+            <h2 id="booking-small-container"className="booking-header">Book an appointment</h2>
+
+            
+            <div id="booking-small-container"className="selection-container">
+                <form>
+                    <p>Select date and time:</p>
+                    <input type="datetime-local"/>
+                    
+                    <p>Select cleaning type:</p>
+                    <select id="size" name="size">
+                    <option value="standard">Standard</option>
+                    <option value="plus">Plus</option>
+                    <option value="super">Super</option>
+                    </select>
+
+                    <p>Select cleaner:</p>
+                    <select id="size" name="size">
+                    <option value="rickard">Rickard</option>
+                    <option value="bosse">Bosse</option>
+                    <option value="Agneta">Agneta</option>
+                    </select>
+                    
+                    <br/>
+                    <button>Book</button>
+
+                </form>
+            </div>
+
         </div>
     )
 }
--- src/components/customerPage/components/CustomerCompleted.tsx
+++ src/components/customerPage/components/CustomerCompleted.tsx
@@ -5,6 +5,40 @@ function CustomerCompleted () {
     return(
         <div className="completed-container">
             <h2>Completed Appointments</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                    <th>Time and Date</th>
+                    <th>Cleaner</th>
+                    <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>March 15, 2023 10:00 AM</td>
+                        <td>Rickard</td>
+                        <td>Plus</td>
+                    </tr>
+                    <tr>
+                        <td>March 16, 2023 2:00 PM</td>
+                        <td>Rickard</td>
+                        <td>Standard</td>
+                    </tr>
+                    <tr>
+                        <td>March 18, 2023 9:30 AM</td>
+                        <td>Agneta</td>
+                        <td>Super</td>
+                    </tr>
+                    <tr>
+                        <td>March 19, 2023 3:00 PM</td>
+                        <td>Bosse</td>
+                        <td>Standard</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+
         </div>
     )
 }
