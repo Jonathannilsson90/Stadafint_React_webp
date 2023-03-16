59b8ff760eef8a35153710b98eb037bea2a6c323
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-03-16 14:13:27 +0100

    Params

--- src/components/customerPage/components/CustomerHero.tsx
+++ src/components/customerPage/components/CustomerHero.tsx
@@ -1,18 +1,18 @@
 import { useLocation } from "react-router-dom";
 import '../styles/CustomerHero.css';
+import { useParams } from "react-router-dom"
 import CleanerScrub from '../../../assets/CleanerScrub.png'
 
 function CustomerHero () {
 
-    const location = useLocation();
-    const data = location.state;
+    let {name} = useParams()
 
     return(
     
     <div className="hero">
             <div className="main-hero">
 
-            <h1>Welcome {data}!</h1>
+            <h1>Welcome {name}!</h1>
             <p>Hurry and get your chance for <br/> the most incredible cleaning service <br/> you've ever experienced</p>
 
             <img src= {CleanerScrub} alt="Hero picture"></img>
