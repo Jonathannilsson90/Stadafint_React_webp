2093c737cdcf8c229df0febadd51f71ab648394c
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-03-27 16:10:35 +0200

    Dödspushen+RIP

--- src/components/customerPage/CustomerPage.tsx
+++ src/components/customerPage/CustomerPage.tsx
@@ -4,21 +4,21 @@ import CustomerButtonBar from './components/CustomerButtonBar';
 import CustomerBooking from './components/CustomerBooking';
 import CustomerAppointments from './components/CustomerAppointments'
 import CustomerCompleted from './components/CustomerCompleted'
-import APIContext from './api';
+
 
 //TODO: FIX CSS BY COMPILING
 
 const CustomerPage = () => {
-    const apiUrl = 'https://stadafint-server-production.up.railway.app/'
+   
     return(
         <div className="main-page">
-            <APIContext.Provider value={{apiUrl}}>
+      
             <CustomerHero />
             <CustomerButtonBar />
             <CustomerBooking />
             <CustomerAppointments />
             <CustomerCompleted />
-            </APIContext.Provider>
+          
         </div>
     )
 }
